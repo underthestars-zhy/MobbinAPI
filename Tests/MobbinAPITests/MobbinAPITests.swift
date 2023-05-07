0d970b4256795133a377ae98cde893fd5215038c
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2023-05-07 15:47:54 +0800

    update test

--- Tests/MobbinAPITests/MobbinAPITests.swift
+++ Tests/MobbinAPITests/MobbinAPITests.swift
@@ -13,14 +13,23 @@ final class MobbinAPITests: XCTestCase {
 
     func testLogin() async throws {
         let api = MobbinAPI(email: "zhuhaoyu0909@icloud.com")
-        let res = try await api.verify(code: "827080")
+        let res = try await api.verify(code: "790680")
         print(api.token?.accessToken ?? "No Token")
+        print(api.token?.refreshToken ?? "No Refresh Token")
 
         XCTAssertTrue(res)
     }
 
+    func testRefresh() async throws {
+        let api = MobbinAPI(email: "zhuhaoyu0909@icloud.com", token: Token(accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjgzNTIxNzg4LCJzdWIiOiJkMmI0MTU5MC01M2E0LTRjNGQtOWQ5NC0wNmY1ZDcxODA3YzMiLCJlbWFpbCI6InpodWhhb3l1MDkwOUBpY2xvdWQuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6eyJhdmF0YXJfdXJsIjoiaHR0cHM6Ly91amFzbnRrZnBoeXdpenNkYWFwaS5zdXBhYmFzZS5jby9zdG9yYWdlL3YxL29iamVjdC9wdWJsaWMvdXNlci91c2VyX2F2YXRhcnMvYTcyZTgwNjQtZjdlYi00YjBhLTg0MWMtMzlhM2ExMGRmY2E0LmpwZWciLCJmdWxsX25hbWUiOiJVVFN6aHkifSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJvdHAiLCJ0aW1lc3RhbXAiOjE2ODM0MzUzODh9XSwic2Vzc2lvbl9pZCI6IjQzMzZkOGYyLTkzOTgtNGNjNi04MDNiLTRhZmYyNmIxOGY4YiJ9.KVvFSOddlMBusP9_BzAbfwuEt7O5o06tX4glI_LLM1I", refreshToken: "vr2WYWgBztAABdypZlAK1Q"))
+
+        try await api.refreshToken()
+
+        print(try api.retrieveToken())
+    }
+
     func testGetiOSCount() async throws {
-        let api = MobbinAPI(email: "zhuhaoyu0909@icloud.com", token: Token(accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjgyNjcxODMzLCJzdWIiOiJkMmI0MTU5MC01M2E0LTRjNGQtOWQ5NC0wNmY1ZDcxODA3YzMiLCJlbWFpbCI6InpodWhhb3l1MDkwOUBpY2xvdWQuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6eyJhdmF0YXJfdXJsIjoiaHR0cHM6Ly91amFzbnRrZnBoeXdpenNkYWFwaS5zdXBhYmFzZS5jby9zdG9yYWdlL3YxL29iamVjdC9wdWJsaWMvdXNlci91c2VyX2F2YXRhcnMvYTcyZTgwNjQtZjdlYi00YjBhLTg0MWMtMzlhM2ExMGRmY2E0LmpwZWciLCJmdWxsX25hbWUiOiJVVFN6aHkifSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJvdHAiLCJ0aW1lc3RhbXAiOjE2ODI1ODU0MzN9XSwic2Vzc2lvbl9pZCI6IjVjMDgzMTA4LWQ3MGItNDRjYy1hN2E1LTczZTNjNjIwYWIzOCJ9.xtwVizdrSwVnObhZ4AClqqzsq-q0mn5UTsuYgShtF7E", refreshToken: ""))
+        let api = MobbinAPI(email: "zhuhaoyu0909@icloud.com", token: Token(accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjgzNTIxNzg4LCJzdWIiOiJkMmI0MTU5MC01M2E0LTRjNGQtOWQ5NC0wNmY1ZDcxODA3YzMiLCJlbWFpbCI6InpodWhhb3l1MDkwOUBpY2xvdWQuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6eyJhdmF0YXJfdXJsIjoiaHR0cHM6Ly91amFzbnRrZnBoeXdpenNkYWFwaS5zdXBhYmFzZS5jby9zdG9yYWdlL3YxL29iamVjdC9wdWJsaWMvdXNlci91c2VyX2F2YXRhcnMvYTcyZTgwNjQtZjdlYi00YjBhLTg0MWMtMzlhM2ExMGRmY2E0LmpwZWciLCJmdWxsX25hbWUiOiJVVFN6aHkifSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJvdHAiLCJ0aW1lc3RhbXAiOjE2ODM0MzUzODh9XSwic2Vzc2lvbl9pZCI6IjQzMzZkOGYyLTkzOTgtNGNjNi04MDNiLTRhZmYyNmIxOGY4YiJ9.KVvFSOddlMBusP9_BzAbfwuEt7O5o06tX4glI_LLM1I", refreshToken: "vr2WYWgBztAABdypZlAK1Q"))
 
         let appsCount = try await api.iOSAppsCount
         let screensCount = try await api.iOSScreensCount
